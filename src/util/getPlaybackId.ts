116ddde262ec0031568b7dfbff7af41f2a229a48
James Homer <james@homerjam.es>
2022-10-03 15:50:03 +0100

    Update getPlaybackId.ts
    Signed-off-by: James Homer <james@homerjam.es>

--- src/util/getPlaybackId.ts
+++ src/util/getPlaybackId.ts
@@ -3,7 +3,7 @@ import type {VideoAssetDocument} from './types'
 export function getPlaybackId(asset: VideoAssetDocument): string {
   if (!asset?.playbackId) {
     console.error('Asset is missing a playbackId', {asset})
-    throw new TypeError(`Missing playbackId`)
+    // throw new TypeError(`Missing playbackId`)
   }
   return asset.playbackId
 }
